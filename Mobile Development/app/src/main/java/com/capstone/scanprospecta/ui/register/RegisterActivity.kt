33fe0a69d29e266f3b50b2d3805ae1034f0edcc5
Dhavina Ocxa <dhavinaoxca123@gmail.com>
2023-12-12 22:09:15 +0700

    onboarding, login, regis

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/register/RegisterActivity.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/register/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package com.capstone.scanprospecta.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.scanprospecta.R
+import com.capstone.scanprospecta.databinding.ActivityRegisterBinding
+import com.capstone.scanprospecta.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
