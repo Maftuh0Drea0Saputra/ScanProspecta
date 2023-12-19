62002f702dffd87b49d55a2970852afb2a5b2614
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-19 23:26:34 +0700

    Update RegisterViewModel.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/register/RegisterViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/register/RegisterViewModel.kt
@@ -4,6 +4,6 @@ import androidx.lifecycle.ViewModel
 import com.capstone.scanprospecta.data.UserRepository
 
 class RegisterViewModel (private val repository: UserRepository): ViewModel() {
-    fun register(name: String,email: String, password: String) = repository.signup(name,email,password)
+    fun register(name: String,email: String, password: String) = repository.register(name,email,password)
 
 }
