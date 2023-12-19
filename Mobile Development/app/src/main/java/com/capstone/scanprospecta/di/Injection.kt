a9354d9b97d1181dcc797952c3ffaf3630bbf5fb
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-19 23:26:44 +0700

    Create Injection.kt

---
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/di/Injection.kt
@@ -0,0 +1,15 @@
+package com.capstone.scanprospecta.di
+
+import android.content.Context
+import com.capstone.scanprospecta.data.UserRepository
+import com.capstone.scanprospecta.data.api.ApiConfig
+import com.capstone.scanprospecta.data.preference.UserPreference
+import com.capstone.scanprospecta.data.preference.dataStore
+
+object Injection {
+    fun provideRepository(context: Context): UserRepository {
+        val preference = UserPreference.getInstance(context.dataStore)
+        val apiService = ApiConfig.getApiService()
+        return UserRepository.getInstance(apiService)
+    }
+}
