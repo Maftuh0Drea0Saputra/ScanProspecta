d56b928eabd587a5139fba35d8b30a37b6997354
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-20 02:19:40 +0700

    Update HomeAdapter.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/home/HomeAdapter.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/home/HomeAdapter.kt
@@ -4,21 +4,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.capstone.scanprospecta.data.response.LoginResponse
 
-class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ListViewHolder>() {
+class HomeAdapter () {
     class ListViewHolder {
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
 
 }
