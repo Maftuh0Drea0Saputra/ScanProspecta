c3f5ec3f56c0aa67c034e13367905e83da52449f
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-20 02:19:31 +0700

    Update MessageResponse.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/response/MessageResponse.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/response/MessageResponse.kt
@@ -2,6 +2,7 @@ package com.capstone.scanprospecta.data.response
 
 import com.google.firebase.database.IgnoreExtraProperties
 
+
 @IgnoreExtraProperties
 data class Message(
     val text: String? = null,
