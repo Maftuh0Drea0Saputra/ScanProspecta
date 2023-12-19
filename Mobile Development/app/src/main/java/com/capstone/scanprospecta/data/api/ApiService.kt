004eff6629f327014a2e6e286337c3d136baee7d
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-20 02:19:26 +0700

    Update ApiService.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/api/ApiService.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/api/ApiService.kt
@@ -2,20 +2,23 @@ package com.capstone.scanprospecta.data.api
 
 import com.capstone.scanprospecta.data.response.LoginResponse
 import com.capstone.scanprospecta.data.response.RegisterResponse
+import retrofit2.http.Body
 import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
-    @FormUrlEncoded
-    @POST("register")
+//    @FormUrlEncoded
+    @POST("authentication/register")
     suspend fun register(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field("password") password: String
+        @Body requestBody: Map<String, String>
+//        @Query("name") name: String,
+//        @Query("email") email: String,
+//        @Query("password") password: String,
+//        @Query("conf_password") conf_passwrod: String
     ): RegisterResponse
 
-    @FormUrlEncoded
+//    @FormUrlEncoded
     @POST("login")
     suspend fun login(
         @Field("email") email: String,
