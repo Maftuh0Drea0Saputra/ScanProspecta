a34a72dce87f6fa35c215785644942e4f7c5758d
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-20 20:47:48 +0700

    Update LoginViewModel.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/login/LoginViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/login/LoginViewModel.kt
@@ -1,4 +1,17 @@
 package com.capstone.scanprospecta.ui.login
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.capstone.scanprospecta.data.preference.UserModel
+import com.capstone.scanprospecta.data.repository.UserRepository
+import kotlinx.coroutines.launch
+
+class LoginViewModel (private val repository: UserRepository): ViewModel() {
+    fun saveSession(user: UserModel) {
+        viewModelScope.launch {
+            repository.saveSession(user)
+        }
+    }
+    fun login(requestBody: Map<String, String>) = repository.login(requestBody)
+
 }
