5fc4d0379b1c03dfedfac4e7de20ef8659507800
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 02:36:37 +0700

    Update MainViewModel.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/main/MainViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/main/MainViewModel.kt
@@ -3,11 +3,15 @@ package com.capstone.scanprospecta.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.capstone.scanprospecta.data.repository.UserRepository
 import com.capstone.scanprospecta.data.preference.UserModel
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: UserRepository) : ViewModel() {
-    fun getSession(): LiveData<String> {
+    fun getSession(): LiveData<UserModel> {
         return repository.getSession().asLiveData()
     }
+
+
 }
