f6831a0ab2a39e6d998c4d7f31fabf097ad9084d
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 13:09:08 +0700

    Update ApiService.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/api/ApiService.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/api/ApiService.kt
@@ -1,20 +1,41 @@
 package com.capstone.scanprospecta.data.api
 
+import com.capstone.scanprospecta.data.response.JobRecomResponse
 import com.capstone.scanprospecta.data.response.LoginResponse
 import com.capstone.scanprospecta.data.response.RegisterResponse
+import com.capstone.scanprospecta.data.response.ResumeResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
+import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface ApiService {
-    @POST("/authentication/register")
+    @POST("authentication/register")
     suspend fun register(
         @Body requestBody: Map<String, String>
     ): RegisterResponse
 
-
-    @POST("/authentication/login")
+    @POST("authentication/login")
     suspend fun login(
         @Body requestBody: Map<String, String>
     ): LoginResponse
+
+    @Multipart
+    @POST("predict")
+    suspend fun postJobRecom(
+        @Part file: MultipartBody.Part,
+        @Part("predictions") predictions: RequestBody
+    ): JobRecomResponse
+
+    @GET("resume_example")
+    fun getResumeExample(
+        @Header("Authorization") token: String
+    ): ResumeResponse
 }
