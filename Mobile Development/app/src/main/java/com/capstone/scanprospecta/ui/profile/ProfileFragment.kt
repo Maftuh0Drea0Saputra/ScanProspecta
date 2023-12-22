192c2e980ae5191cadf94d6a5e916a44e80bff0a
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 13:10:05 +0700

    Update ProfileFragment.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/profile/ProfileFragment.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.capstone.scanprospecta.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.capstone.scanprospecta.databinding.FragmentProfileBinding
 import com.capstone.scanprospecta.ui.ViewModelFactory
+import com.capstone.scanprospecta.ui.login.LoginActivity
 
 class ProfileFragment : Fragment() {
 
@@ -28,15 +30,16 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        viewModel.getSession().observe(viewLifecycleOwner) { user ->
-//            binding.apply {
-//          //      tvName.text = user.name
-//          //      tvEmail.text = user.email
-////                tvEmail.text = user.email
-//            }
-//        }
+        viewModel.getSession().observe(viewLifecycleOwner) { user ->
+            binding.apply {
+                tvName.text = user.name
+                tvEmail.text = user.email
+            }
+        }
         binding.btnLogout.setOnClickListener {
             viewModel.logout()
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
