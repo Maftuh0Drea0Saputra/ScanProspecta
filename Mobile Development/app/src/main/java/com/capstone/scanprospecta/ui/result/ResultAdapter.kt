1b9f16f117b0a0293176614aa8fc66083acf3c84
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 16:08:00 +0700

    Update ResultAdapter.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/result/ResultAdapter.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/result/ResultAdapter.kt
@@ -1,30 +1,41 @@
 package com.capstone.scanprospecta.ui.result
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.capstone.scanprospecta.data.response.DetailItem
 import com.capstone.scanprospecta.databinding.ItemResultBinding
 
-class ResultAdapter(
-    private val context: Context,
-    private val data: List<String>
-): RecyclerView.Adapter<ResultAdapter.MyViewHolder>() {
+class ResultAdapter : ListAdapter<DetailItem, ResultAdapter.MyViewHolder>(DIFF_CALLBACK){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val binding = ItemResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val result = data
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val result = getItem(position)
         holder.bind(result)
     }
 
-    override fun getItemCount() = data.size
+//    override fun getItemCount() = data.size
 
     class MyViewHolder(val binding: ItemResultBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(result: List<String>){
+        fun bind(result: DetailItem){
             binding.tvJob.text = result.toString()
+        }
+    }
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DetailItem>() {
+            override fun areItemsTheSame(oldItem: DetailItem, newItem: DetailItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: DetailItem, newItem: DetailItem): Boolean {
+                return oldItem == newItem
+            }
         }
     }
 }
