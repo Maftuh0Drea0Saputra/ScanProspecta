2a7a7a2fa7edc30a6214345fc7f2bcf2092ecefd
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 13:10:23 +0700

    Update ResumeViewModel.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/resume/ResumeViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/resume/ResumeViewModel.kt
@@ -1,13 +1,66 @@
 package com.capstone.scanprospecta.ui.resume
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.capstone.scanprospecta.data.ResultState
+import com.capstone.scanprospecta.data.preference.UserPreference
+import com.capstone.scanprospecta.data.repository.ResumeRepository
+import com.capstone.scanprospecta.data.response.DataItem
+import com.capstone.scanprospecta.di.Injection
+import kotlinx.coroutines.flow.map
 
-class ResumeViewModel : ViewModel() {
+class ResumeViewModel(
+    private val repository: ResumeRepository,
+    private val preference: UserPreference
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun getResumeExample(token: String): LiveData<ResultState<List<DataItem>>> {
+        return repository.getResumeExample(token)
     }
-    val text: LiveData<String> = _text
+
+    fun checkTokenAvailable(): LiveData<String> {
+        return preference.getSession()
+            .map { userModel ->
+                userModel.token
+            }
+            .asLiveData()
+    }
+
+    class ResumeViewModelFactory private constructor (
+        private val repository: ResumeRepository,
+        private val preference: UserPreference
+    ):
+        ViewModelProvider.NewInstanceFactory() {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ResumeViewModel::class.java)) {
+                return ResumeViewModel(repository, preference) as T
+            }
+
+            throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        }
+
+        companion object {
+            @Volatile
+            private var INSTANCE: ResumeViewModelFactory? = null
+            @JvmStatic
+            fun getInstance(
+                context: Context,
+                preference: UserPreference
+            ): ResumeViewModelFactory {
+                if (INSTANCE == null) {
+                    synchronized(ResumeViewModelFactory::class.java) {
+                        INSTANCE = ResumeViewModelFactory(
+                            Injection.provideResumeRepository(),
+                            preference
+                        )
+                    }
+                }
+                return INSTANCE as ResumeViewModelFactory
+            }
+        }
+    }
 }
