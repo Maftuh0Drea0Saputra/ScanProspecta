55ae92204235579496311abb1b96851482947aa4
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 13:10:32 +0700

    Update Utils.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/utils/Utils.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/utils/Utils.kt
@@ -34,40 +34,46 @@ fun createFile(application: Application): File {
     return File(outputDirectory, "StoryApp$timeStamp.jpg")
 }
 
-fun Bitmap.getRotatedBitmap(file: File): Bitmap? {
-    val orientation = ExifInterface(file).getAttributeInt(
-        ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED
-    )
-    return when (orientation) {
-        ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(this, 90F)
-        ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(this, 180F)
-        ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(this, 270F)
-        ExifInterface.ORIENTATION_NORMAL -> this
-        else -> this
+fun rotateBitmap(bitmap: Bitmap, isBackCamera: Boolean = false): Bitmap {
+    val matrix = Matrix()
+    return if (isBackCamera) {
+        matrix.postRotate(90f)
+        Bitmap.createBitmap(
+            bitmap,
+            0,
+            0,
+            bitmap.width,
+            bitmap.height,
+            matrix,
+            true
+        )
+    } else {
+        matrix.postRotate(-90f)
+        matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
+        Bitmap.createBitmap(
+            bitmap,
+            0,
+            0,
+            bitmap.width,
+            bitmap.height,
+            matrix,
+            true
+        )
     }
 }
 
-fun rotateImage(source: Bitmap, angle: Float): Bitmap? {
-    val matrix = Matrix()
-    matrix.postRotate(angle)
-    return Bitmap.createBitmap(
-        source, 0, 0, source.width, source.height, matrix, true
-    )
-}
-
-fun File.reduceFileImage(): File {
-    val file = this
-    val bitmap = BitmapFactory.decodeFile(file.path).getRotatedBitmap(file)
+fun reduceFileImage(file: File): File {
+    val bitmap = BitmapFactory.decodeFile(file.path)
     var compressQuality = 100
     var streamLength: Int
     do {
         val bmpStream = ByteArrayOutputStream()
-        bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
         val bmpPicByteArray = bmpStream.toByteArray()
         streamLength = bmpPicByteArray.size
         compressQuality -= 5
-    } while (streamLength > MAXIMAL_SIZE)
-    bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
+    } while (streamLength > 1000000)
+    bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
     return file
 }
 
