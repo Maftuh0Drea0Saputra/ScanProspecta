b5fa604156419e1903d99972163bee20b39aa463
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 13:10:08 +0700

    Update ProfileViewModel.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/profile/ProfileViewModel.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/ui/profile/ProfileViewModel.kt
@@ -11,6 +11,9 @@ import kotlinx.coroutines.launch
 
 class ProfileViewModel(private val repository: UserRepository) : ViewModel() {
 
+    fun getSession(): LiveData<UserModel> {
+        return  repository.getSession().asLiveData()
+    }
     fun logout() {
         viewModelScope.launch {
             repository.logout()
