9e572855696a74d7aa8af4b6d4d0e58a4c780ccc
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 16:07:56 +0700

    Update JobRecomResponse.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/response/JobRecomResponse.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/data/response/JobRecomResponse.kt
@@ -4,6 +4,24 @@ import com.google.gson.annotations.SerializedName
 
 data class JobRecomResponse(
 
-    @field:SerializedName("predictions")
-    val predictions: List<String>
+	@field:SerializedName("detail")
+	val detail: List<DetailItem?>? = null
+)
+
+data class DetailItem(
+
+	@field:SerializedName("msg")
+	val msg: String? = null,
+
+	@field:SerializedName("loc")
+	val loc: List<String?>? = null,
+
+	@field:SerializedName("input")
+	val input: Any? = null,
+
+	@field:SerializedName("type")
+	val type: String? = null,
+
+	@field:SerializedName("url")
+	val url: String? = null
 )
