7c593076b5c3b958e4333296cd83ca4513dcc0ee
Maftuh Drea <maftuhdrea@gmail.com>
2023-12-22 13:09:40 +0700

    Update Injection.kt

--- Mobile Development/app/src/main/java/com/capstone/scanprospecta/di/Injection.kt
+++ Mobile Development/app/src/main/java/com/capstone/scanprospecta/di/Injection.kt
@@ -1,15 +1,28 @@
 package com.capstone.scanprospecta.di
 
 import android.content.Context
-import com.capstone.scanprospecta.data.api.ApiConfig
 import com.capstone.scanprospecta.data.repository.UserRepository
+import com.capstone.scanprospecta.data.api.apiServiceEndpoint1
+import com.capstone.scanprospecta.data.api.apiServiceEndpoint2
 import com.capstone.scanprospecta.data.preference.UserPreference
 import com.capstone.scanprospecta.data.preference.dataStore
+import com.capstone.scanprospecta.data.repository.JobRecomRepository
+import com.capstone.scanprospecta.data.repository.ResumeRepository
 
 object Injection {
-    fun provideRepository(context: Context): UserRepository {
+    fun provideUserRepository(context: Context): UserRepository {
         val preference = UserPreference.getInstance(context.dataStore)
-        val apiService = ApiConfig.getApiService()
+        val apiService = apiServiceEndpoint1
         return UserRepository.getInstance(preference,apiService)
+    }
+
+    fun provideJobRecomRepository(): JobRecomRepository {
+        val apiService = apiServiceEndpoint2
+        return JobRecomRepository.getInstance(apiService)
+    }
+
+    fun provideResumeRepository(): ResumeRepository {
+        val apiService = apiServiceEndpoint1
+        return ResumeRepository.getInstance(apiService)
     }
 }
